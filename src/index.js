661770e92981995a4e4c25bd21c62d259fc759f4
PolinaBel <120118029+pollyBe@users.noreply.github.com>
2024-10-22 15:51:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let length = str.length;
+  let length1 = bracketsConfig.length;
+  let arr = [];
+
+  if (length % 2 !== 0) {
+    return false;
+  }
+  if (length == 0) {
+    return false;
+  }
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length1; j++) {
+      let conf = arr[arr.length - 1];
+
+      if (
+        str[i] == bracketsConfig[j][1] &&
+        arr.length != 0 &&
+        conf == bracketsConfig[j][0]
+      ) {
+        arr.pop();
+      } else if (str[i] == bracketsConfig[j][0]) {
+        arr.push(str[i]);
+      }
+    }
+  }
+
+  if (arr.length == 0) {
+    return true;
+  }
+  if (arr.length != 0) {
+    return false;
+  }
+};
